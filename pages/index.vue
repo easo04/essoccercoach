764c5a06cde121fbae6fae7f1e156fd9a8ce6b5d
easo02@hotmail.fr <easo02@hotmail.fr>
2021-01-29 14:26:42 -0500

    add btn scroll top

--- pages/index.vue
+++ pages/index.vue
@@ -206,6 +206,21 @@ export default {
     this.lstSlidesAlignements = slidesAlignements;
   },
   mounted(){
+    $(document).ready(function(){
+      $('.btn-top').click(function(){
+        $('body, html').animate({
+          scrollTop: '0px'
+        }, 300);
+      });
+    
+      $(window).scroll(function(){
+        if( $(this).scrollTop() > 0 ){
+          $('.btn-top').slideDown(300);
+        } else {
+          $('.btn-top').slideUp(300);
+        }
+      });
+    });
   }
 }
 </script>
