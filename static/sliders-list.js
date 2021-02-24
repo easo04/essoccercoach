e9c55e37f766de5217cf44562cbe039d9785c718
easo02@hotmail.fr <easo02@hotmail.fr>
2021-02-24 11:37:57 -0500

    add images modal open slide designer

--- static/sliders-list.js
+++ static/sliders-list.js
@@ -8,16 +8,16 @@ export const lstSlidersModalOpenDesigner = [
         text:'Vous pouvez sélectionner le terrain, les joueurs, les outils et les fleches à partir du ménu de gauche.'
     },
     {
-        image:'open-designer1.png',
-        text:'text2'
+        image:'open-designer2.png',
+        text:'Vous pouvez ajouter du texte et le déplacer à l\'endroit de votre choix. Vous pouvez également changer la couleur du texte.'
     },
     {
-        image:'open-designer1.png',
-        text:'text3'
+        image:'open-designer3.gif',
+        text:'Vous pouvez ajouter des formes à votre exercice, changer leurs couleurs et ajuster leurs dimensions.'
     },
     {
-        image:'open-designer1.png',
-        text:'text4'
+        image:'open-designer4.png',
+        text:'Vous pouvez télécharger votre exercice en cliquant sur l\'icône à droit du menu principale.'
     },
 ];
 
